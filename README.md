8002c260ee81cf5029045797ef1ddae85d9d6753
Karl Lehenbauer <karl-gh@lehenbauer.com>
2015-07-29 06:18:23 +0000

    example showing how "info exists a(b)" won't compile

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 This example shows how to use upvar to pass the name of an array into a tsp::proc definition, a common usage pattern in Tcl.
 
+## array_broken.tcl
+
+This example attempts to add "info exists" to check on the presence of an array element fails to compile.
+
 ## lrandom.tcl
 
 This provides an lrandrom proc cribbed from the Tcl wiki and a tsp::proc equivalent and times runs of the two.
---
+++ array_broken.tcl
@@ -0,0 +1,52 @@
+#
+# Tcl Static Prime example referencing an array using upvar
+#
+# Note that the passed-in array name, _array, is defined as a var in
+# the #tsp::procdef and "array", which _array is upvar'ed to, is
+# defined as a tsp::array
+#
+# THIS ONE HAS PROBLEMS
+#
+# the call to "info exists" fails because the argument is an array reference.
+# info exists on array elements is going to be hard to do without
+#
+# also "incr array($key) $value" doesn't work because tsp wants a scalar
+# for the variable name.  this is worked around in the code below.
+#
+#
+
+package require tsp
+
+tsp::proc aset {_array key value} {
+	#tsp::procdef var -args var var int
+	#tsp::compile assert
+	#tsp::array array
+	#tsp::boolean b
+	#tsp::int i
+	
+	upvar $_array array
+
+	set b [info exists array($key)]
+	if {$b} {
+
+		#incr array($key) $value
+		set i $array($key)
+		set array($key) [expr {$i + $value}]
+
+	} else {
+		set array($key) $value
+	}
+
+	return $array($key)
+}
+
+aset foo a 10
+aset foo b 10
+aset foo c 10
+
+aset foo b 10
+
+aset foo c 10
+aset foo c 10
+
+parray foo
